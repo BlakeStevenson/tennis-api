dac463fe1258fa78464f59f20d4e5d2882550393
Blake Stevenson <24233354+BlakeStevenson@users.noreply.github.com>
2020-10-10 16:20:35 -0500

    port 3401

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ app.set('json spaces', 2);
 require('./routes/usta')(app);
 
 // start server
-app.listen(process.env.EXPRESS_PORT, () => console.log("Listening on port " + process.env.EXPRESS_PORT));
+app.listen(3401, () => console.log("Listening on port 3401"));
